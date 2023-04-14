c4890c59b219400e257bf200623212b080838a09
JapanCodeman <ryansurdick@MacBook-Pro.local>
2023-04-14 20:29:30 +0900

    experimented with a few different libraries; added header and PicAndWelcome components and assigned color variables

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header-text'>Ryan Surdick</div>
+    </div>
+  );
+}
+export default Header;
---
+++ src/components/PicAndWelcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PicAndWelcome() {
+  return (
+    <div className='pic-and-welcome-wrap'>
+      <img className='welcome-image' src='https://placehold.jp/250x25p.png' alt="placeholder" />
+      <div className='welcome-text'>This is where the welcome message will go</div>
+    </div>
+  );
+}
+export default PicAndWelcome;
